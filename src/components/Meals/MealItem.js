eebdaf2c005aaa736c463209e3cbfa9c7f818727
Harsh2813 <prateektiwari.pk55@gmail.com>
2023-12-18 18:27:24 +0530

    added button and reusable UI input component and rendered in MealItemForm

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = (props) => {
 
     let price = `$${props.price.toFixed(2)}`;
-    
+
   return (
     <li className={classes.meal}>
       <div>
@@ -12,7 +13,7 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div> <MealItemForm/> </div>
     </li>
   )
 }
