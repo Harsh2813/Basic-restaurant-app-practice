da803b955882fa50ef34e21373f06c0644ca7412
Harsh2813 <prateektiwari.pk55@gmail.com>
2023-12-18 16:39:30 +0530

    added summary

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food Delivery to you</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et asperiores aliquam eligendi ea optio tenetur doloremque impedi.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum tempora dicta dolor minima? Officiis, quod!</p>
+    </section>
+  )
+}
+
+export default MealsSummary
